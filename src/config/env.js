5f348ef20288f154fc2109f80c96d1c53fc5289c
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-08-06 18:07:30 +0200

    feat: added more variables and config files

---
+++ src/config/env.js
@@ -0,0 +1,6 @@
+export const env = {
+    "PORT": parseInt(process.env.PORT) || "PORT not set",
+    "SALT_ROUNDS": parseInt(process.env.SALT_ROUNDS) || 10,
+    "JWT_ACCESS_TOKEN_SECRET": process.env.JWT_TOKEN_SECRET || "JWT_ACCESS_TOKEN_SECRET not set",
+    "JWT_REFRESH_TOKEN_SECRET": process.env.JWT_REFRESH_TOKEN_SECRET || "JWT_REFRESH_TOKEN_SECRET not set",
+}
