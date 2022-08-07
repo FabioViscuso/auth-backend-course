859e15351a1f18b882f698ed122878074e559b47
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-08-07 14:46:04 +0200

    fix: cleaned up old files and adjust dockerfile to user postgres14

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
-import Database from "./database/db.js"
-import { env } from "./config/env.js";
-import { dbConfig } from "./config/dbconfig.js";
+import Database from "./database";
+import environment from "./config/environment";
+import dbConfig from "./config/database";
 
+// IIFE : immediately invoked function express
 (async () => {
     try {
-        const db = new Database(env, dbConfig);
-        await db.connectToDB();
+        const db = new Database(environment.nodeEnv, dbConfig);
+        await db.connect();
     } catch (err) {
         console.log('error while connecting to db', err.stack)
     }
